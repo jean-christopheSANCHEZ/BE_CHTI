96f92dd36a67adf0189fb6d7cba227affb3d06b5
Onemaeus97 <58248979+Onemaeus97@users.noreply.github.com>
2020-03-27 16:10:02 +0100

    etape2

--- etape2-19-3/PROJ_UN/DFT.s
+++ etape2-19-3/PROJ_UN/DFT.s
@@ -11,26 +11,6 @@ re_im proc
 	push {r6}
 	push {r7}
 	push {r8}
-	mov r6, #0 							;variable d'incrémentation
-	mov	r8, #0							;somme
-loop	ldrsh r5, [r0, r6, LSL #1] 		;on stocke x(i)
-	mul r7, r6, r1 						;on calcule i*k
-	and	r7, #0x3F 						;i*k mod 64
-	ldrsh r12, [r2, r7, LSL #1]			;sin(i*k*2pi/N) = tabsin(i*k mod 64), idem pour cos
-	mla r8, r5, r12, r8 				;on continue la somme : +(x(i)*tabsin(i*k))
-	
-	add r6, #1
-	cmp r6, #64
-	blo loop 
-	
-	mov r0, r8
-	pop {r8}
-	pop {r7}
-	pop {r6}
-	pop {r5}
-	pop {r4}
+	mov r6, #0 							
+	mov	r8, #0							
 
-fin	bx	lr								;dernière instruction de la fonction
-	endp
-;
-	end
